033e05f21aa34297de833ca829f3b4afe2f3dc53
Jason Gillman Jr <jgillman@liquidweb.com>
2020-02-29 15:23:08 -0500

    byte dumper

--- utils.py
+++ utils.py
@@ -12,3 +12,13 @@ def convert_magic(input: str):
         raise Exception('Ooops. This should probably 4 characters...')
     converted_magic = (ord(input[0]) << 24) | (ord(input[1]) << 16) | (ord(input[2]) << 8) | ord(input[3])
     return converted_magic
+
+def byte_dump(input: int) -> bytes:
+    """
+    Dump bytes from an int.
+    Big endian
+    :param input:
+    :return:
+    """
+    byte_length = (input.bit_length() + 7) // 8
+    return input.to_bytes(byte_length, 'big')
